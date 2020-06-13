ff22b762c4c6ad3200d7afc6da6cdd9efdc938d0
Darryl.Mendonez <darryl.mendonez@Laptop.home>
2020-06-12 22:41:16 -0400

    set up redux saga

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
+import { loadImages } from '../../actions'
 import './styles.css';
 
 const key = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
@@ -38,6 +39,7 @@ class ImageGrid extends Component {
                             />
                         </div>
                     ))}
+                    <a onClick={this.props.loadImages}>Load more</a>
                 </section>
             </div>
         );
@@ -50,7 +52,11 @@ const mapStateToProps = ({ isLoading, images, error }) => ({
     error,
 })
 
+const mapDispatchToProps = dispatch => ({
+    loadImages: () => dispatch(loadImages()),
+})
+
 export default connect(
     mapStateToProps,
-    null
+    mapDispatchToProps,
 )(ImageGrid);
---
+++ src/sagas/index.js
@@ -0,0 +1,14 @@
+import { takeEvery } from 'redux-saga/effects'
+
+import { IMAGES } from '../constants'
+
+function* handleImagesLoad() {
+  console.log('fetching images from unsplash')
+}
+
+// watcher saga
+function* rootSaga() {
+  yield takeEvery(IMAGES.LOAD, handleImagesLoad)
+}
+
+export default rootSaga
